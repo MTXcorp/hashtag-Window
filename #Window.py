e0b119d0298160c2e5b6b6145281033c968abb26
Eryk Patocki <dunno8489@hotmail.com>
2024-01-28 12:10:35 +0100

    Added The File "#Window.py", Fixed Bugs, Added the "supercmd" Command, Added The File "F#Window.py".

---
+++ #Window.py
@@ -0,0 +1,50 @@
+import os as sys
+
+SessionStatus = "define"
+Version = " V2.0"
+Name = "#Window"
+TypeCmd = "in cmd"
+rootUserName = "change_me"
+rootPassword = "changeme"
+
+
+
+
+
+def clean():
+    if sys.name == "nt":
+        sys.system("cls")
+    else:
+        sys.system("clear")
+def Folder():
+    sys.system("dir")
+def text_edit():
+    clean()
+SessionStatus = "Cleaning"
+clean()
+SessionStatus = "in cmd"
+print("#Window V2.0")
+while SessionStatus == "in cmd":
+    cmdinput = input("|>")
+    if cmdinput.lower() == "version":
+        print(Name + Version)
+    if cmdinput.lower() == "exit":
+        clean()
+        exit("Exiting Python And #Window...")
+    if cmdinput.lower() == "supercmd " + "exit":
+        hashcallinput = input("|Password of " + rootUserName + ":|>")
+        if hashcallinput.lower() == rootPassword:
+           clean()
+           exit("[root]" + "Exiting Python And #Window...")
+        else:
+            print("Try Again!")
+    if cmdinput.lower() == "files":
+        sys.system("dir")
+    if cmdinput.lower() == "supercmd " + "files":
+        hashcallinput = input("|Password of " + rootUserName + ":|>")
+        if hashcallinput == rootPassword:
+            sys.system("dir")
+        else:
+            print("Try Again!")
+    if cmdinput.lower() == "supercmd " + "version":
+        hashcallinput = input("|Password of " + rootUserName + ":|>")
---
+++ F#Window/F#Window.py
@@ -0,0 +1,5 @@
+#This Is The F#Window Framework. It Is Used With the #Window OS.
+#To Access Help Visit The "Help.html" file.
+import os as sys
+
+Version = "v1.0"
